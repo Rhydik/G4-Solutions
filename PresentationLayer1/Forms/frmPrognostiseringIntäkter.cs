f2e2582bb861a2d205d3dd9df45d177f676631d0
Danny Chiem <danny_lc_@hotmail.com>
2019-10-23 15:19:17 +0200

    Sista formsen skapade.

---
+++ PresentationLayer1/Forms/frmPrognostiseringIntäkter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer1.Forms
+{
+    public partial class frmPrognostiseringIntäkter : Form
+    {
+        public frmPrognostiseringIntäkter()
+        {
+            InitializeComponent();
+        }
+
+        private void lblKategori_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbKategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

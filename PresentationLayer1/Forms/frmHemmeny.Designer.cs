eb358e4ddacc7e7f9a68f24c426e2ce5f5c86560
Marcussss <thetrickyest@gmail.com>
2019-10-30 15:04:45 +0100

    fixa en liten skitsak

--- PresentationLayer1/Forms/frmHemmeny.Designer.cs
+++ PresentationLayer1/Forms/frmHemmeny.Designer.cs
@@ -6,12 +6,12 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        /// <param name="disposing">true if managed resources should be disposed;
+     
+ protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
@@ -19,198 +19,55 @@
             }
             base.Dispose(disposing);
         }
-
         #region Windows Form Designer generated code
-
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHemmeny));
+            System.ComponentModel.ComponentResourceManager resources = new
+           System.ComponentModel.ComponentResourceManager(typeof(frmHemmeny));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.ucMeny1 = new PresentationLayer1.Forms.ucMeny();
+
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.btnResultat);
-            this.panel1.Controls.Add(this.btnPrognostiseringIntäkter);
-            this.panel1.Controls.Add(this.btnBudgeteratResultat);
-            this.panel1.Controls.Add(this.btnKostnadsbudgetering);
-            this.panel1.Controls.Add(this.btnIntäktsbudgeteringProdukt);
-            this.panel1.Controls.Add(this.btnIntäktsbudgeteringKund);
-            this.panel1.Controls.Add(this.btnBehörighet);
-            this.panel1.Controls.Add(this.btnSchablonkostnad);
-            this.panel1.Controls.Add(this.btnAktiviteter);
-            this.panel1.Controls.Add(this.btnPersonal);
-            this.panel1.Controls.Add(this.btnProdukter);
-            this.panel1.Controls.Add(this.btnKunder);
-            this.panel1.Location = new System.Drawing.Point(7, 6);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(188, 432);
-            this.panel1.TabIndex = 3;
-            // 
-            // btnResultat
-            // 
-            this.btnResultat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnResultat.Location = new System.Drawing.Point(15, 381);
-            this.btnResultat.Name = "btnResultat";
-            this.btnResultat.Size = new System.Drawing.Size(75, 23);
-            this.btnResultat.TabIndex = 2;
-            this.btnResultat.Text = "Resultat";
-            this.btnResultat.UseVisualStyleBackColor = true;
-            // 
-            // btnPrognostiseringIntäkter
-            // 
-            this.btnPrognostiseringIntäkter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPrognostiseringIntäkter.Location = new System.Drawing.Point(15, 352);
-            this.btnPrognostiseringIntäkter.Name = "btnPrognostiseringIntäkter";
-            this.btnPrognostiseringIntäkter.Size = new System.Drawing.Size(141, 23);
-            this.btnPrognostiseringIntäkter.TabIndex = 2;
-            this.btnPrognostiseringIntäkter.Text = "Prognostisering intäkter";
-            this.btnPrognostiseringIntäkter.UseVisualStyleBackColor = true;
-            // 
-            // btnBudgeteratResultat
-            // 
-            this.btnBudgeteratResultat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBudgeteratResultat.Location = new System.Drawing.Point(15, 322);
-            this.btnBudgeteratResultat.Name = "btnBudgeteratResultat";
-            this.btnBudgeteratResultat.Size = new System.Drawing.Size(123, 23);
-            this.btnBudgeteratResultat.TabIndex = 2;
-            this.btnBudgeteratResultat.Text = "Budgeterat Resultat";
-            this.btnBudgeteratResultat.UseVisualStyleBackColor = true;
-            // 
-            // btnKostnadsbudgetering
-            // 
-            this.btnKostnadsbudgetering.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnKostnadsbudgetering.Location = new System.Drawing.Point(15, 274);
-            this.btnKostnadsbudgetering.Name = "btnKostnadsbudgetering";
-            this.btnKostnadsbudgetering.Size = new System.Drawing.Size(158, 23);
-            this.btnKostnadsbudgetering.TabIndex = 2;
-            this.btnKostnadsbudgetering.Text = "Kostnadsbudgetering";
-            this.btnKostnadsbudgetering.UseVisualStyleBackColor = true;
-            // 
-            // btnIntäktsbudgeteringProdukt
-            // 
-            this.btnIntäktsbudgeteringProdukt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIntäktsbudgeteringProdukt.Location = new System.Drawing.Point(15, 245);
-            this.btnIntäktsbudgeteringProdukt.Name = "btnIntäktsbudgeteringProdukt";
-            this.btnIntäktsbudgeteringProdukt.Size = new System.Drawing.Size(158, 23);
-            this.btnIntäktsbudgeteringProdukt.TabIndex = 2;
-            this.btnIntäktsbudgeteringProdukt.Text = "Intäktsbudgetering Produkt";
-            this.btnIntäktsbudgeteringProdukt.UseVisualStyleBackColor = true;
-            // 
-            // btnIntäktsbudgeteringKund
-            // 
-            this.btnIntäktsbudgeteringKund.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIntäktsbudgeteringKund.Location = new System.Drawing.Point(15, 216);
-            this.btnIntäktsbudgeteringKund.Name = "btnIntäktsbudgeteringKund";
-            this.btnIntäktsbudgeteringKund.Size = new System.Drawing.Size(158, 23);
-            this.btnIntäktsbudgeteringKund.TabIndex = 3;
-            this.btnIntäktsbudgeteringKund.Text = "Intäktsbudgetering Kund";
-            this.btnIntäktsbudgeteringKund.UseVisualStyleBackColor = true;
-            this.btnIntäktsbudgeteringKund.Click += new System.EventHandler(this.btnIntäktsbudgeteringKund_Click);
-            // 
-            // btnBehörighet
-            // 
-            this.btnBehörighet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBehörighet.Location = new System.Drawing.Point(15, 169);
-            this.btnBehörighet.Name = "btnBehörighet";
-            this.btnBehörighet.Size = new System.Drawing.Size(107, 23);
-            this.btnBehörighet.TabIndex = 2;
-            this.btnBehörighet.Text = "Behörighet";
-            this.btnBehörighet.UseVisualStyleBackColor = true;
-            this.btnBehörighet.Click += new System.EventHandler(this.btnBehörighet_Click);
-            // 
-            // btnSchablonkostnad
-            // 
-            this.btnSchablonkostnad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSchablonkostnad.Location = new System.Drawing.Point(15, 140);
-            this.btnSchablonkostnad.Name = "btnSchablonkostnad";
-            this.btnSchablonkostnad.Size = new System.Drawing.Size(107, 23);
-            this.btnSchablonkostnad.TabIndex = 2;
-            this.btnSchablonkostnad.Text = "Schablonkostnad";
-            this.btnSchablonkostnad.UseVisualStyleBackColor = true;
-            this.btnSchablonkostnad.Click += new System.EventHandler(this.btnSchablonkostnad_Click);
-            // 
-            // btnAktiviteter
-            // 
-            this.btnAktiviteter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAktiviteter.Location = new System.Drawing.Point(15, 110);
-            this.btnAktiviteter.Name = "btnAktiviteter";
-            this.btnAktiviteter.Size = new System.Drawing.Size(107, 23);
-            this.btnAktiviteter.TabIndex = 2;
-            this.btnAktiviteter.Text = "Aktiviteter";
-            this.btnAktiviteter.UseVisualStyleBackColor = true;
-            this.btnAktiviteter.Click += new System.EventHandler(this.btnAktiviteter_Click);
-            // 
-            // btnPersonal
-            // 
-            this.btnPersonal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPersonal.Location = new System.Drawing.Point(15, 82);
-            this.btnPersonal.Name = "btnPersonal";
-            this.btnPersonal.Size = new System.Drawing.Size(107, 23);
-            this.btnPersonal.TabIndex = 2;
-            this.btnPersonal.Text = "Personal";
-            this.btnPersonal.UseVisualStyleBackColor = true;
-            this.btnPersonal.Click += new System.EventHandler(this.btnPersonal_Click);
-            // 
-            // btnProdukter
-            // 
-            this.btnProdukter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnProdukter.Location = new System.Drawing.Point(15, 53);
-            this.btnProdukter.Name = "btnProdukter";
-            this.btnProdukter.Size = new System.Drawing.Size(107, 23);
-            this.btnProdukter.TabIndex = 2;
-            this.btnProdukter.Text = "Produkter";
-            this.btnProdukter.UseVisualStyleBackColor = true;
-            this.btnProdukter.Click += new System.EventHandler(this.btnProdukter_Click);
-            // 
-            // btnKunder
-            // 
-            this.btnKunder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnKunder.Location = new System.Drawing.Point(15, 23);
-            this.btnKunder.Name = "btnKunder";
-            this.btnKunder.Size = new System.Drawing.Size(107, 23);
-            this.btnKunder.TabIndex = 2;
-            this.btnKunder.Text = "Kunder";
-            this.btnKunder.UseVisualStyleBackColor = true;
-            this.btnKunder.Click += new System.EventHandler(this.btnKunder_Click);
-            // 
+            //
             // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            //
+            this.pictureBox1.Anchor =
+           ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top |
+           System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image =
+           ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(277, 91);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(454, 221);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
-            // 
+            //
             // splitter1
-            // 
+            //
             this.splitter1.Location = new System.Drawing.Point(0, 0);
             this.splitter1.Margin = new System.Windows.Forms.Padding(2);
             this.splitter1.Name = "splitter1";
             this.splitter1.Size = new System.Drawing.Size(2, 450);
             this.splitter1.TabIndex = 4;
             this.splitter1.TabStop = false;
-            // 
+            //
             // ucMeny1
-            // 
+            //
             this.ucMeny1.Location = new System.Drawing.Point(8, 8);
             this.ucMeny1.Name = "ucMeny1";
             this.ucMeny1.Size = new System.Drawing.Size(194, 438);
             this.ucMeny1.TabIndex = 5;
-            // 
+            //
             // frmHemmeny
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
@@ -220,12 +77,9 @@
             this.Name = "frmHemmeny";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "G4 Solutions Economy System";
-            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-
         }
-
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Splitter splitter1;

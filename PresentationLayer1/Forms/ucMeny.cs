2c692ccc54d0dc5318b93ec8d644ce0d777d2a55
Jacob Bergqvist <Jacob.bergqvist@gmail.com>
2019-10-30 14:04:11 +0100

    ucMeny asdfasdf

--- PresentationLayer1/Forms/ucMeny.cs
+++ PresentationLayer1/Forms/ucMeny.cs
@@ -20,49 +20,50 @@ namespace PresentationLayer1.Forms
         private void btnKunder_Click(object sender, EventArgs e)
         {
             frmKunder frmKunder = new frmKunder();
-            Hide();
+            (Application.OpenForms["frmHemmeny"] as frmHemmeny).Hide();
+
             frmKunder.Show();
         }
 
         private void btnProdukter_Click(object sender, EventArgs e)
         {
             Forms.frmProdukter frmProdukter = new Forms.frmProdukter();
-            Hide();
+            (Application.OpenForms["frmHemmeny"] as frmHemmeny).Hide();
             frmProdukter.Show();
         }
 
         private void btnPersonal_Click(object sender, EventArgs e)
         {
             Forms.frmPersonal frmPersonal = new Forms.frmPersonal();
-            Hide();
+            (Application.OpenForms["frmHemmeny"] as frmHemmeny).Hide();
             frmPersonal.Show();
         }
 
         private void btnAktiviteter_Click(object sender, EventArgs e)
         {
             Forms.frmAktiviteter frmAktiviteter = new Forms.frmAktiviteter();
-            Hide();
+            (Application.OpenForms["frmHemmeny"] as frmHemmeny).Hide();
             frmAktiviteter.Show();
         }
 
         private void btnSchablonkostnad_Click(object sender, EventArgs e)
         {
             Forms.frmSchablonkostnad frmSchablonkostnad = new Forms.frmSchablonkostnad();
-            Hide();
+            (Application.OpenForms["frmHemmeny"] as frmHemmeny).Hide();
             frmSchablonkostnad.Show();
         }
 
         private void btnBehörighet_Click(object sender, EventArgs e)
         {
             Forms.frmBehörighet frmbehörighet = new Forms.frmBehörighet();
-            Hide();
+            (Application.OpenForms["frmHemmeny"] as frmHemmeny).Hide();
             frmbehörighet.Show();
         }
 
         private void btnIntäktsbudgeteringKund_Click(object sender, EventArgs e)
         {
-            this.Visible = !this.Visible;
             Forms.frmIntäktsbudgeteringKund frmIntäktsbudgeteringKund = new Forms.frmIntäktsbudgeteringKund();
+            (Application.OpenForms["frmHemmeny"] as frmHemmeny).Hide();
             frmIntäktsbudgeteringKund.Show();
         }
     }

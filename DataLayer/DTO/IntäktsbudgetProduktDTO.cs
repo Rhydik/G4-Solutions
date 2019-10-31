6e06ef5bff1b39151f330f5cf20d1f91cd72d017
Jacob Bergqvist <Jacob.bergqvist@gmail.com>
2019-10-31 15:16:55 +0100

    Intäktsbudget produkt DTO

---
+++ DataLayer/DTO/IntäktsbudgetProduktDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.DTO
+{
+    public class IntäktsbudgetProduktDTO
+    {
+        public string KundID { get; set; }
+        public string KundNamn { get; set; }
+        public decimal Avtal { get; set; }
+        public bool GradA { get; set; }
+        public decimal Tillägg { get; set; }
+        public bool GradT { get; set; }
+        public decimal Budget { get; set; }
+        public int Tim { get; set; }
+        public string Kommentar { get; set; }
+    }
+}
